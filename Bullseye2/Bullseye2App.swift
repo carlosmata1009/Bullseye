fbbe1e3313ced3ce298d8f71e41b082121520027
Carlos Mata <carlosmata1009@gmail.com>
2023-03-06 13:01:34 +0100

    Initial Commit

---
+++ Bullseye2/Bullseye2App.swift
@@ -0,0 +1,17 @@
+//
+//  Bullseye2App.swift
+//  Bullseye2
+//
+//  Created by Carlos Mata on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Bullseye2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
